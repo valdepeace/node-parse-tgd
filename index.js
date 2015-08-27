0fb50f616e957f3333304e8e524ce8ba28070e87
Andres Carmona Gil <valdepeace@gmail.com>
2015-08-27 19:59:55 +0200

    resolve problem with file path

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 /**
  * Created by Andres Carmona Gil on 26/08/2015.
  */
-    'use strict'
+'use strict'
 
 var java = require("java");
+var path = require("path");
+var fs = require("fs");
+
 java.classpath.push("commons-lang3-3.1.jar");
 java.classpath.push("commons-io.jar");
 java.classpath.push("./lib-java/lib-tgd.jar");
-java.classpath.push("./lib-java/jackson-annotations-2.5.1.jar");
-java.classpath.push("./lib-java/jackson-core-2.5.1.jar");
-java.classpath.push("./lib-java/jackson-databind-2.5.1.jar");
+java.classpath.push(path.resolve(__dirname, "./lib-java/lib-tgd.jar"));
+java.classpath.push(path.resolve(__dirname, "./lib-java/jackson-annotations-2.5.1.jar"));
+java.classpath.push(path.resolve(__dirname, "./lib-java/jackson-core-2.5.1.jar"));
+java.classpath.push(path.resolve(__dirname, "./lib-java/jackson-databind-2.5.1.jar"));
 
 /*
  * module.exports
  */
 
-module.exports = function(buffer) {
+function tgd(buffer) {
+
     buffer.toByteArray = function () {
         return Array.prototype.slice.call(this, 0)
     };
     // Recupero el array de bytes del buffer
-    var bytes=bufffer.toByteArray();
+    var bytes = buffer.toByteArray();
     // Construyo el array de bytes de javascript a java
-    var arraybyte=java.newArray("byte",bytes);
-    //instancio la clase
-    var filetgd=java.newInstanceSync("org.tacografo.file.FileBlockTGD");
-    java.newInstance("org.tacografo.file.FileBlockTGD",function(err,filetgd){});
-    // Importo la instancia de la clase para usarla
-    var FileTGD=java.import("org.tacografo.file.FileBlockTGD");
+    var arraybyte = java.newArray("byte", bytes);
+    // Importo la clase para usarla
+    var FileTGD = java.import("org.tacografo.file.FileBlockTGD");
     // La Instancio para que llame al constructor con el array de bytes java
-    var tgd=new FileTGD(arraybyte);
+    var tgd = new FileTGD(arraybyte);
     // por fin llamo al metodo getjson
-    var parseo=JSON.parse(tgd.getJsonSync());
 
-    return parseo
+    var parsing = JSON.parse(tgd.getJsonSync());
+
+    return parsing;
 };
+
+
+module.exports = tgd;
