70304340d38956d0885eee8e2723ef53d32eb1eb
Andres Carmona Gil <valdepeace@gmail.com>
2015-11-11 20:55:30 +0100

    add test file exemple

--- test/test.js
+++ test/test.js
@@ -1,4 +1,23 @@
 /**
  * Created by negrero on 27/08/2015.
  */
-require
+/**
+ * Created by Andres Carmona Gil on 26/08/2015.
+ */
+'use strict';
+var fs=require('fs')
+var fileTGD=require('../index')
+
+fs.readFile('path/file.tgd',
+    function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    fileTGD(data,function(err,result){
+        if (err){
+            console.log(err)
+        }else{
+            console.log(result)
+        }
+    });
+});
